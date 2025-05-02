8df71b017473e11a92b92e3f1076fadbb43985bf
Andy Guajardo <andymatt1225@gmail.com>
2025-05-01 23:22:07 -0700

    front end folder structure created, webpack created and functioning

---
+++ client/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <div>
+      <h1>Anime & Manga Tracker</h1>
+      <p>Welcome! Start building your list.</p>
+    </div>
+  );
+};
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+const path = require('path');
+
+module.exports = {
+  entry: './client/src/index.jsx',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: '/'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: 'babel-loader'
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.js', '.jsx']
+  },
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'client/public')
+    },
+    historyApiFallback: true,
+    port: 3000,
+    open: true
+  },
+  mode: 'development'
+};
